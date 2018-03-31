6f214708d8b3ea8515b0c1fc5065b18a3d9eb7f4
Akash Chavan <meakashchavan@gmail.com>
2018-04-01 01:04:12 +0530

    update urls of respective views

---
+++ app/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'app'
+
+urlpatterns = [
+    # ex: /app/
+    url(
+        r'^$',
+        views.index,
+        name='index'
+    ),
+    url(
+        r'^(?P<question_id>[0-9]+)/$',
+        views.detail,
+        name='detail'
+    ),
+    url(
+        r'^(?P<question_id>[0-9]+)/vote/$',
+        views.vote,
+        name='vote'
+    ),
+
+]
