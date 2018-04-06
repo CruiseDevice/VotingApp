8534c7049b937b065da17ad98fa0ca45eaf5eeee
Akash Chavan <meakashchavan@gmail.com>
2018-04-07 02:00:26 +0530

    pub_date for question

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 import datetime
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.encoding import python_2_unicode_compatible
@@ -10,7 +11,15 @@ from django.utils.encoding import python_2_unicode_compatible
 class Question(models.Model):
     # pass
     question_text = models.CharField(max_length=200, default="")
-    pub_date = models.DateField('date published')
+    pub_date = models.DateTimeField(
+        blank=True, 
+        null=True,
+        auto_now_add=True
+        )
+
+    def publish(self):
+        self.pub_date = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.question_text
