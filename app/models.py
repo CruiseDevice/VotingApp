2448e09300124a7b62f664b9d39381dbdc1818e6
Akash Chavan <meakashchavan@gmail.com>
2018-04-14 00:08:01 +0530

    create association between User and Question

--- app/models.py
+++ app/models.py
@@ -6,10 +6,12 @@ from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.encoding import python_2_unicode_compatible
+from accounts.models import User
 
 @python_2_unicode_compatible
 class Question(models.Model):
     # pass
+    user = models.ForeignKey(User,blank=True, null=True)
     question_text = models.CharField(max_length=200, default="")
     pub_date = models.DateTimeField(
         blank=True, 
@@ -22,7 +24,7 @@ class Question(models.Model):
         self.save()
 
     def __str__(self):
-        return self.question_text
+        return '{} by {}'.format(self.user, self.question_text)
 
 class Choice(models.Model):
     # pass
