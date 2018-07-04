eb6ca01234a1b5f4ac33b1e557432decc75be2a8
CruiseDevice <meakashchavan@gmail.com>
2018-07-05 00:22:28 +0530

    use form set to add choices while adding question

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from app.models import Question, Choice
+from django.forms.widgets import RadioSelect
+from django.forms import inlineformset_factory
 
 class QuestionForm(forms.ModelForm):
     class Meta:
@@ -10,5 +12,27 @@ class QuestionForm(forms.ModelForm):
 class ChoiceForm(forms.ModelForm):
     class Meta:
         model = Choice
-        fields = ('question','choice_text',)
+        fields = ('choice_text',)
 
+ChoiceFormSet = inlineformset_factory(Question, Choice, form=ChoiceForm, extra=4)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Repeat password',
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ('username','first_name','email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
