63df9b2330540eaef9f05d952888f9dcfc4ed5d5
CruiseDevice <meakashchavan@gmail.com>
2018-09-14 14:27:44 +0530

    clean models

--- app/models.py
+++ app/models.py
@@ -26,39 +26,12 @@ class Question(models.Model):
         blank=True, 
         null=True,
         auto_now_add=True
-        )
-    choice_order = models.CharField(
-        max_length=30, 
-        null=True, 
-        blank=True,
-        choices = CHOICE_ORDER_OPTIONS,
-        help_text=_("The order in which choices are"
-                    " displayed to the user."),
-        verbose_name=_("Choice Order")
     )
 
     def publish(self):
         self.pub_date = timezone.now()
         self.save()
 
-    def order_choices(self, queryset):
-        if self.choice_order == 'content':
-            return queryset.order_by('content')
-        if self.choice_order == 'random':
-            return queryset.order_by('?')
-        if self.choice_order == 'none':
-            return queryset.order_by()
-        return queryset
-
-    def get_choices(self):
-        return self.order_choices(Choice.objects.filter(question=self))
-
-    def get_choices_list(self):
-        return [(choice.id, choice.choice_text)for choice in self \
-                .order_choices(Choice.objects.filter(question=self))]
-
-    def choice_to_string(self, guess):
-        return Choice.objects.get(id=guess).choice_text
 
     def __str__(self):
         return '{} by {}'.format(self.question_text, self.user)
